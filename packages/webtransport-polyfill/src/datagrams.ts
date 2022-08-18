6669b1ddca5370df98d88ee47c63074b48e4bcb6
LuZhenqian <luzhenqian98@gmail.com>
2022-08-18 14:57:44 +0800

    fix: re writer and re reader

--- packages/webtransport-polyfill/src/datagrams.ts
+++ packages/webtransport-polyfill/src/datagrams.ts
@@ -1,28 +1,43 @@
-import { Iterator } from './iterator';
-
 export class DataGrams {
-  writable: WritableStream<any>;
-  readable: ReadableStream<any>;
-  // #iterator: Iterator<Uint8Array>;
+  writable: WritableStream<Uint8Array>;
+  readable: ReadableStream<Uint8Array>;
   constructor(wss: WebSocket) {
-    this.writable = new WritableStream<Uint8Array>({
-      write(chunk) {
-        return new Promise((resolve, reject) => {
-          try {
-            wss.send(chunk);
-            resolve();
-          } catch (e) {
-            reject(e);
-          }
-        });
-      },
-    });
+    return new Proxy(this, {
+      get(target, prop, receiver) {
+        if (prop === 'writable') {
+          return new WritableStream<Uint8Array>({
+            start(controller) {
+              console.log('start');
+            },
+            write(chunk) {
+              return new Promise((resolve, reject) => {
+                try {
+                  wss.send(chunk);
+                  resolve();
+                } catch (e) {
+                  reject(e);
+                }
+              });
+            },
+            close() {
+              console.log('close');
 
-    this.readable = new ReadableStream({
-      start(controller) {
-        wss.addEventListener('message', ev => {
-          controller.enqueue(ev.data);
-        });
+              return new Promise((resolve, reject) => {
+                resolve();
+              });
+            },
+            abort(reason) {},
+          });
+        } else if (prop === 'readable') {
+          return new ReadableStream({
+            start(controller) {
+              wss.addEventListener('message', ev => {
+                controller.enqueue(ev.data);
+              });
+            },
+          });
+        }
+        return undefined;
       },
     });
   }
