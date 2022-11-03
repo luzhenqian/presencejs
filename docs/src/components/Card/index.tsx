f5e362b267717e310c74b6d3d6cc47880fbcd7c5
LuZhenqian <luzhenqian98@gmail.com>
2022-11-04 01:00:07 +0800

    feat: adjust sidebar, cart add href

--- docs/src/components/Card/index.tsx
+++ docs/src/components/Card/index.tsx
@@ -1,12 +1,15 @@
+import Link from '@docusaurus/Link';
 import React from 'react';
 
-export const Card = ({ style = {}, children }) => {
+export const Card = ({ href = '#', style = {}, children }) => {
   return (
-    <div
-      className="inline-flex justify-center w-40 p-10 border rounded-md shadow-sm cursor-pointer"
-      style={style}
-    >
-      {children}
-    </div>
+    <Link to={href}>
+      <div
+        className="inline-flex justify-center w-40 p-10 border rounded-md shadow-sm cursor-pointer"
+        style={style}
+      >
+        {children}
+      </div>
+    </Link>
   );
 };
