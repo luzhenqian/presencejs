530d765ec6725a99caa16d959ed52a802e52d1d8
LuZhenqian <luzhenqian98@gmail.com>
2022-11-24 11:34:33 +0800

    feat: group hug adjust style

--- components/react/group-hug/entry.tsx
+++ components/react/group-hug/entry.tsx
@@ -1,11 +1,14 @@
-import { createPresence } from '@yomo/presence';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { createPresence } from '@yomo/presence';
+import '@yomo/webtransport-polyfill';
 import GroupHug from './index';
+
 const domContainer = document.querySelector('#app');
 const root = createRoot(domContainer);
 
 const id = Math.random().toString();
+const avatar = `https://robohash.org/${id}`;
 const presence = createPresence({
   url: 'https://prscd2.allegro.earth/v1',
   publicKey: 'BYePWMVCfkWRarcDLBIbSFzrMkDldWIBuKsA',
@@ -13,4 +16,4 @@ const presence = createPresence({
   appId: 'lzq',
 });
 
-root.render(<GroupHug presence={presence} id={id} />);
+root.render(<GroupHug presence={presence} id={id} avatar={avatar} />);
--- components/react/group-hug/types.ts
+++ components/react/group-hug/types.ts
@@ -3,5 +3,8 @@ import { IPresence } from '@yomo/presence';
 export type GroupHugProps = {
   presence: Promise<IPresence>;
   avatar?: string;
+  avatarBorderColor?: string;
   id: string;
+  size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+  grouped?: boolean;
 };
